dc053ece78dbdd88c965857435641a7debb547e6
RSkooTy <127528782+RSkooTy@users.noreply.github.com>
2023-11-24 23:32:27 +0300

    Add files via upload

--- laba7.java
+++ laba7.java
@@ -1,30 +1,30 @@
-public class laba7 {
-    private int element;
+public class laba7<T> {
+    private T element;
 
-    public laba7(int element){
+    public laba7(T element) {
         this.element = element;
     }
 
-    public int getElement() {
+    public T getElement() {
         return element;
     }
 
-    public void setElement(int element) {
+    public void setElement(T element) {
         this.element = element;
     }
 
-    public static void tradeElement(laba7 firstElement, laba7 secondElement) {
-        int trd = firstElement.getElement();
-        firstElement.setElement(secondElement.getElement());
-        secondElement.setElement(trd);
+    public static <T> void tradeElement(laba7<T> firstContainer, laba7<T> secondContainer) {
+        T temp = firstContainer.getElement();
+        firstContainer.setElement(secondContainer.getElement());
+        secondContainer.setElement(temp);
     }
 
     public static void main(String[] args) {
-        laba7 firstElements = new laba7(1);
-        laba7 secondElements = new laba7(2);
+        laba7<Integer> firstContainer = new laba7<>(1);
+        laba7<Integer> secondContainer = new laba7<>(2);
 
-        System.out.println("Before trade: " + "First: " + firstElements.getElement() + " Second: " + secondElements.getElement());
-        tradeElement(firstElements, secondElements);
-        System.out.println("After trade: " + "First: " + firstElements.getElement() + " Second: " + secondElements.getElement());
+        System.out.println("Before trade: " + "First: " + firstContainer.getElement() + " Second: " + secondContainer.getElement());
+        tradeElement(firstContainer, secondContainer);
+        System.out.println("After trade: " + "First: " + firstContainer.getElement() + " Second: " + secondContainer.getElement());
     }
 }
